e209fa98bdf3a9dea74d7d03f94f742cd0f72863
uselogin <uselogin93@gmail.com>
2026-01-02 06:49:47 +0000

    message

--- first-app/componets/Navbar.js
+++ first-app/componets/Navbar.js
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar Component
+    <div className='w-screen h-[10vh] bg-blue-900 text-white flex flex-col justify-center items-center'>
+      <div className='w-[49%] h-[99%] flex justify-center items-center'>
+      <h1 className='text-3xl font-bold'>Navbar</h1>
+      </div>
+      <div className='w-[49%] h-[99%] flex justify-center items-center'>
+        <ul className='flex space-x-4 ml-10  flex-col justify-center items-center md:flex-row md:space-x-10 text-lg font-medium'>
+          <li className='hover:underline cursor-pointer'>Home</li>
+          <li className='hover:underline cursor-pointer'>About</li>
+          <li className='hover:underline cursor-pointer'>Contact</li>
+        </ul>
+      </div>
     </div>
   )
 }
