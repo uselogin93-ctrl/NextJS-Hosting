85b19d547513b80837a0c0abfc4577f193a3a534
uselogin <uselogin93@gmail.com>
2026-01-02 13:51:50 +0000

    message

--- first-app/app/page.js
+++ first-app/app/page.js
@@ -1,15 +1,17 @@
 import Image from "next/image";
 
-export default async function Home() { 
-  
-  let data = await fetch('https://api.vercel.app/blog',  { next: { revalidate: 3600 } })
+export default async function Home() {
+
+  let data = await fetch('https://api.vercel.app/blog', { next: { revalidate: 3600 } })
   let posts = await data.json()
   return (
-    <ul>
-      {posts.map((post) => (
-        <li key={post.id}>{post.title}</li>
-      ))}
-    </ul>
+    <div className="w-screen h-[90vh] flex flex-col justify-center items-center bg-blue-950 text-white">
+      <ul className="mb-8 text-3xl font-bold w-[40%] h-[50%] overflow-y-auto">
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
