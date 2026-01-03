86edda9aec604e714dbe131516746dba9ea275c0
uselogin <uselogin93@gmail.com>
2026-01-03 14:43:13 +0000

    message

--- three-next-app/app/page.js
+++ three-next-app/app/page.js
@@ -2,11 +2,12 @@
 import Image from "next/image";
 
 export default function Home() {
-  console.log("This ID use ",process.env.ID," for login");
+  // console.log("This ID use ",process.env.ID," for login");
   return (
     <div>
       <h1>THis is Home Pages</h1>
-      <p>This ID use "{process.env.ID}" for login</p>
+      <p>This ID use "{process.env.NEXT_PUBLIC_ID}" for login</p>
+      <p>This ID use "{process.env.NEXT_PUBLIC_SECRET}" for login</p>
     </div>
   );
 }
