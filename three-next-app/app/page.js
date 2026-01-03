a05ce8d20432f8c3a3b58c304003ca5a5cd704c7
uselogin <uselogin93@gmail.com>
2026-01-03 14:55:38 +0000

    message

--- three-next-app/app/page.js
+++ three-next-app/app/page.js
@@ -2,7 +2,8 @@
 import Image from "next/image";
 
 export default function Home() {
-  // console.log("This ID use ",process.env.ID," for login");
+  console.log("This ID use ",process.env.NEXT_PUBLIC_ID," for login");
+  console.log("This Password use ",process.env.NEXT_PUBLIC_SECRET," for login");
   return (
     <div>
       <h1>THis is Home Pages</h1>
