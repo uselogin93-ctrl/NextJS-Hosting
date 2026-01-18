19b5401232d400f510490f7ef2508f1044d2d894
uselogin <uselogin93@gmail.com>
2026-01-18 06:03:49 +0000

    message

--- four-next-app/app/page.js
+++ four-next-app/app/page.js
@@ -1,65 +1,64 @@
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  let putData = async (id,thumbnail,title,description,date,url) => {
+    new=[
+      "id":id,
+      "thumbnail":thumbnail,
+      "title":title,
+      "description":description,
+      "date":date,
+      "url":url
+    ]
+  }
+
+  let data = [];
+  try {
+    let response = await fetch('https://www.freetogame.com/api/games?platform=pc');
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    data = await response.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="w-screen h-auto min-h-screen grid grid-cols-4 ">
+      <div>
+        <iframe
+          width="200"
+          height="300"
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&mute=1&loop=1&playlist=dQw4w9WgXcQ"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="autoplay; encrypted-media; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+
+      {data.map((game) => (
+        <div key={game.id} className="m-4 p-4 border-2 border-black rounded-lg w-50 h-100 flex flex-col items-center justify-start gap-4 bg-blue-200">
+          <div className="w-[99%] h-20 flex justify-center items-center">
+            <img src={game.thumbnail} alt={game.title} className="w-[99%] h-[99%]" />
+          </div>
+          <div className="w-[99%] h-10 flex justify-center items-center">
+            <h1>{game.title}</h1>
+          </div>
+          <div className="w-[99%] h-30 flex justify-center items-center">
+            <p>{game.short_description}</p>
+          </div>
+          <div className="w-[99%] h-10 flex justify-center items-center">
+            <p>Release Date: {game.release_date}</p>
+          </div>
+          <div className="w-[99%] h-20 flex justify-center items-center">
+            <button className="rounded-lg w-[80%] h-[80%] bg-black text-white font-bold text-xl">
+              <Link href={{ pathname: `/${game.game_url}`, query: { id: game.id } }}>play</Link>
+            </button>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      ))}
     </div>
   );
 }
